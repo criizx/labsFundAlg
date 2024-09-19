0a09b22f8f765cbcf22f16ccaac302412aa7e140
criizx <164517388+criizx@users.noreply.github.com>
2024-09-19 17:55:21 +0200

    Delete lab1/Point2 directory

--- lab1/Point2/polynomials.h
+++
@@ -1,9 +0,0 @@
-#ifndef POLYNOMIALS_H
-#define POLYNOMIALS_H
-
-double polinomE(double x);
-double polinomPi(double x);
-double polinomLn2(double x);
-double polinomSqrt2(double x);
-
-#endif
--- lab1/Point2/root_finder.h
+++
@@ -1,6 +0,0 @@
-#ifndef ROOT_FINDER_H
-#define ROOT_FINDER_H
-
-double findRoot(double (*polinom)(double), double a, double b, double eps);
-
-#endif
